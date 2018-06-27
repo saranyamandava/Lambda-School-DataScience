ff817b41b161f09384ed1ceb59bef11f6e355506
saranyamandava <37849027+saranyamandava@users.noreply.github.com>
2018-06-27 14:04:29 -0500

    Update note.sql

--- Cloaud AI/SQL Assignment/note.sql
+++ Cloaud AI/SQL Assignment/note.sql
@@ -1,3 +1,49 @@
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128) NOT NULL,
+    release_year INTEGER
+);
+
+CREATE TABLE artist (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE Track(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   Title VARCHAR(128),
+   AlbumId INTEGER NOT NULL,
+   FOREIGN KEY (AlbumId) REFERENCES album(id));
+                                          
+CREATE TABLE artist_album(
+   ArtistId INT NOT NULL,
+   AlbumId  INT NOT NULL,
+   FOREIGN KEY (ArtistId) REFERENCES artist(id),
+   FOREIGN KEY (AlbumId) REFERENCES album(id));
+                                          
+/* Show all albums. */
+select * from album;
+/* Show all albums made between 1975 and 1990. */                                          
+select * from album where release_year >= 1975 and release_year <= 1990;
+ /* Show all albums whose names start with Super D */                                         
+select * from album where title like 'Super D%';
+/* Show all albums that have no release year. */                                          
+SELECT * FROM album WHERE release_year IS NULL;
+/* Show all track titles from Super Funky Album. */                                          
+select Title from Track where AlbumId in (select id from album where title = 'Super Funky Album');
+/* Same query as above, but rename the column from title to Track_Title in the output. */                                          
+select Title as Track_Title from Track where AlbumId in (select id from album where title = 'Super Funky Album');
+/* Select all album titles by Han Solo. */                                          
+select Title from album where id in(select AlbumId from artist_album where ArtistId in (select id from artist where name = 'Han Solo'));select avg(release_year) from album;
+/* Select the average year all albums were released. */
+SELECT AVG(release_year) FROM album;
+/* Select the average year all albums by Leia and the Ewoks were released. */                                                                                        
+select avg(release_year) from album where id in(select AlbumId from artist_album where ArtistId in (select id from artist where name = 'Leia and the Ewoks'));
+/* Select the number of artists. */
+select count(distinct(id)) from artist;
+/* Select the number of tracks on Super Dubstep Album. */
+select count(distinct(id)) from Track where AlbumId in (select id from album where title = 'Super Dubstep Album'); 
+                      
 CREATE TABLE author (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(128) NOT NULL
